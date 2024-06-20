e90fcc753f2294d9612d4ed965ecb739b5227508
Dmitriy Sinitskiy <dmitriy.sinitskiy@quantori.com>
2024-06-20 12:39:53 +0300

    add mods and tests

---
+++ homework_05/src/counters.rs
@@ -0,0 +1,69 @@
+#[allow(dead_code)]
+
+pub type SignedCounter = isize;
+pub type UnsignedCounter = usize;
+
+#[allow(dead_code)]
+pub fn default_signed_counter() -> SignedCounter {
+    0
+}
+
+#[allow(dead_code)]
+pub fn default_unsigned_counter() -> UnsignedCounter {
+    0
+}
+
+#[allow(dead_code)]
+pub fn next_signed(counter: SignedCounter) -> SignedCounter {
+    counter + 1
+}
+
+#[allow(dead_code)]
+pub fn next_unsigned(counter: UnsignedCounter) -> UnsignedCounter {
+    counter + 1
+}
+
+#[allow(dead_code)]
+pub fn prev_signed(counter: SignedCounter) -> SignedCounter {
+    counter - 1
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_default_signed_counter() {
+        let result = default_signed_counter();
+        let expected: SignedCounter = 0;
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_default_unsigned_counter() {
+        let result = default_unsigned_counter();
+        let expected: UnsignedCounter = 0;
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_next_signed() {
+        let result = next_signed(default_signed_counter());
+        let expected: SignedCounter = 1;
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_prev_signed() {
+        let result = prev_signed(default_signed_counter());
+        let expected: SignedCounter = -1;
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_next_unsigned() {
+        let result = next_unsigned(default_unsigned_counter());
+        let expected: UnsignedCounter = 1;
+        assert_eq!(result, expected);
+    }
+}
