045f7344a51faa0636595b643084a2bdb4ca24cb
Dmitriy Sinitskiy <dmitriy.sinitskiy@quantori.com>
2024-07-08 18:37:46 +0300

    add fourth task

--- homework_09/src/task_03.rs
+++ homework_09/src/task_03.rs
@@ -1,1 +1,25 @@
+// 3 Принимает слайс и число N. Возвращает ссылку на N-ый элемент слайса с конца.
 
+pub fn get_element_from_slice<T>(slice: &[T], n: usize) -> Option<&T> {
+    if n < slice.len() {
+        Some(&slice[slice.len() - n - 1])
+    } else {
+        None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_element_from_slice() {
+        let container = [1, 2, 3, 4, 5];
+        let slice = &container[3..5];
+        let result = get_element_from_slice(slice, 1).unwrap();
+
+        let expected = &mut 4;
+
+        assert_eq!(result, expected);
+    }
+}
--- homework_09/src/task_04.rs
+++ homework_09/src/task_04.rs
@@ -1,1 +1,28 @@
+// Принимает слайс и число N. Возвращает два слайса с элементами:
+// с нулевого по N-1;
+// с N-го по последний;
 
+pub fn get_two_elements_from_slice<T>(slice: &[T], n: usize) -> Option<(&[T], &[T])> {
+    if n < slice.len() {
+        Some((&slice[..slice.len() - n - 1], &slice[slice.len() - n - 1..]))
+    } else {
+        None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_two_elements_from_slice() {
+        let container = [1, 2, 3, 4, 5];
+        let slice = &container[..];
+        let result = get_two_elements_from_slice(slice, 2).unwrap();
+
+        let expected_1 = &slice[..2];
+        let expected_2 = &slice[2..];
+
+        assert_eq!(result, (expected_1, expected_2));
+    }
+}
