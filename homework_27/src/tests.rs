5693fa4bf071e8881b443be4db96c97614ff8f39
Dmitriy Sinitskiy <dmitriy.sinitskiy@quantori.com>
2024-11-09 14:23:22 +0400

    add tests for iterator

--- homework_27/src/tests.rs
+++ homework_27/src/tests.rs
@@ -37,3 +37,27 @@ mod tests_len {
         assert_eq!(len, expected);
     }
 }
+
+#[cfg(test)]
+mod tests_iter {
+    use crate::linked_list::LinkedList;
+
+    #[test]
+    fn test_len_zero() {
+        let empty_list: LinkedList<u32> = LinkedList::default();
+        let result: Vec<u32> = empty_list.iter().collect();
+        let expected: Vec<u32> = vec![];
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_len_two() {
+        let mut empty_list: LinkedList<u32> = LinkedList::default();
+        empty_list.push_head(1);
+        empty_list.push_head(10);
+        empty_list.push_head(100);
+        let result: Vec<u32> = empty_list.iter().collect();
+        let expected: Vec<u32> = vec![100, 10, 1];
+        assert_eq!(result, expected);
+    }
+}
