0671c9562ad67328eeadcd69590f6865e2c3b617
Dmitriy Sinitskiy <dmitriy.sinitskiy@quantori.com>
2024-12-06 17:06:30 +0400

    add glitch

--- pyrust/main.py
+++ pyrust/main.py
@@ -1,11 +1,20 @@
 import pyrust
 
+
 with open("images/ararat.png", "rb") as f:
     image_data = f.read()
+
 
 resized_bytes = pyrust.resize_image_from_bytes(image_data, 100, 100, "png")
 
 with open("images/output_resized.png", "wb") as f:
     f.write(resized_bytes)
+
+
+glitched_bytes = pyrust.glitch_effect(image_data, 10, "png")
+
+with open("images/output_glitched.png", "wb") as f:
+    f.write(glitched_bytes)
+
 
 print("Done!")
--- pyrust/src/lib.rs
+++ pyrust/src/lib.rs
@@ -1,22 +1,12 @@
-use image::{DynamicImage, GenericImageView, ImageFormat};
+use image::{DynamicImage, GenericImageView, ImageFormat, RgbaImage, Rgba};
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 use std::fs::File;
 use std::io::{Cursor, Read};
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
 
 #[pyfunction]
-fn resize_image_from_bytes(
-    image_bytes: Vec<u8>,
-    width: u32,
-    height: u32,
-    format: &str,
-) -> PyResult<Vec<u8>> {
+fn resize_image_from_bytes(image_bytes: Vec<u8>, width: u32, height: u32, format: &str,) -> PyResult<Vec<u8>> {
     // Читаем изображение из байтов
     let img = image::load_from_memory(&image_bytes).map_err(|e| {
         pyo3::exceptions::PyIOError::new_err(format!("Failed to load image: {}", e))
@@ -46,10 +36,56 @@ fn resize_image_from_bytes(
     Ok(buffer)
 }
 
+
+#[pyfunction]
+fn glitch_effect(image_bytes: Vec<u8>, offset: u32, format: &str) -> PyResult<Vec<u8>> {
+
+    let img = image::load_from_memory(&image_bytes).map_err(|e| {
+        pyo3::exceptions::PyIOError::new_err(format!("Failed to load image: {}", e))
+    })?;
+    let (width, height) = img.dimensions();
+    let mut new_img: RgbaImage = RgbaImage::new(width, height);
+
+    for y in 0..height {
+        for x in 0..width {
+            let red_x = (x + offset) % width;
+            let green_x = (x + offset / 2) % width;
+            let blue_x = (x + offset / 3) % width;
+
+            let r = img.get_pixel(red_x, y)[0];
+            let g = img.get_pixel(green_x, y)[1];
+            let b = img.get_pixel(blue_x, y)[2];
+
+            new_img.put_pixel(x, y, Rgba([r, g, b, 255]));
+        }
+    }
+    let format = match format.to_lowercase().as_str() {
+        "png" => ImageFormat::Png,
+        "jpeg" | "jpg" => ImageFormat::Jpeg,
+        "bmp" => ImageFormat::Bmp,
+        _ => {
+            return Err(pyo3::exceptions::PyValueError::new_err(
+                "Unsupported format. Use 'png', 'jpeg', or 'bmp'.",
+            ))
+        }
+    };
+
+    let mut buffer = Vec::new();
+    new_img
+        .write_to(&mut Cursor::new(&mut buffer), format)
+        .map_err(|e| {
+            pyo3::exceptions::PyIOError::new_err(format!("Failed to write image: {}", e))
+        })?;
+
+    Ok(buffer)
+}
+
+
 /// A Python module implemented in Rust.
 #[pymodule]
 fn pyrust(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     m.add_function(wrap_pyfunction!(resize_image_from_bytes, m)?)?;
+    m.add_function(wrap_pyfunction!(glitch_effect, m)?)?;
+
     Ok(())
 }
